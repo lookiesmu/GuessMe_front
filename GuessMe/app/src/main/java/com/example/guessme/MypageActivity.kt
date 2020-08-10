369c8063920e2efe2490c9ae551bb9a7f2d5c616
heegeepark <phg980513@naver.com>
2020-08-10 22:34:13 +0900

    declare activities

--- GuessMe/app/src/main/java/com/example/guessme/MypageActivity.kt
+++ GuessMe/app/src/main/java/com/example/guessme/MypageActivity.kt
@@ -1,5 +1,13 @@
 package com.example.guessme
 
-class MypageActivity {
-//    나중에 구현
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MypageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solve_quiz)
+    }
+
 }
