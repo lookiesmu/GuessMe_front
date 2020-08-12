89a80d238514fb67202154c964fd3f2ef8903f14
heegeepark <phg980513@naver.com>
2020-08-12 14:57:01 +0900

    add createquiz get

--- GuessMe/app/src/main/java/com/example/guessme/api/QuizApi.kt
+++ GuessMe/app/src/main/java/com/example/guessme/api/QuizApi.kt
@@ -1,9 +1,11 @@
 package com.example.guessme.api
 
+import com.example.guessme.data.QuizList
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface QuizApi {
     // 퀴즈 생성
-    //@GET("/quizzes")
-    //suspend fun createQuiz() : Post
+    @GET("/quizzes")
+    suspend fun createQuiz() : Response<QuizList>
 }
