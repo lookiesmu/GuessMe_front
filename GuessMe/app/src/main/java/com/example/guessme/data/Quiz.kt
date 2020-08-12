afd8f11f60e6bb1beac76cf134a09a1c89efbfd5
jeongin <jeongiin@naver.com>
2020-08-13 07:14:37 +0900

    ing solve guiz

--- GuessMe/app/src/main/java/com/example/guessme/data/Quiz.kt
+++ GuessMe/app/src/main/java/com/example/guessme/data/Quiz.kt
@@ -3,10 +3,9 @@ package com.example.guessme.data
 import java.io.Serializable
 
 data class Quiz (
-    var quizid: Int, //퀴즈 고유 ID
+    var quizId: Int, //퀴즈 고유 ID
     var content: String, //퀴즈 내용
     var answer: Int //퀴즈 답
 ) : Serializable{
     constructor():this(0,"",0)
-    constructor(content: String):this(0,content,0)
 }
